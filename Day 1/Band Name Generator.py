4aa1693aecb20b942787e03a98a08f6e55c24ad2
EdwinTarus <85441814+EdwinTarus@users.noreply.github.com>
2024-08-06 17:54:04 +0300

    Create Band Name Generator.py

---
+++ Day 1/Band Name Generator.py
@@ -0,0 +1,15 @@
+"""
+This program is for creating a band name using a comnination of two words.
+This is to help learn the concept of string manipulation, variable instantiation,
+and also the print fuction. 
+"""
+
+# Input the two words
+firstName = input("What is the name of the city you grew up in? ")
+lastName = input("What is the name of your pet?")
+
+# Create the band name
+BandName = firstName + " " + lastName
+
+# print function
+print(BandName)
