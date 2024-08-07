28c65718727413c74da61855d3c0694f19fb63f8
EdwinTarus <85441814+EdwinTarus@users.noreply.github.com>
2024-08-07 16:46:26 +0300

    Add files via upload

---
+++ Day 1/Tip calculator.py
@@ -0,0 +1,14 @@
+"""
+Today's lesson is about numbers, operations, type conversion and using the f-strings
+"""
+# Input the bill, the tip percentage, and the number of people to split the bill
+bill = float(input("What is the Bill? "))
+percentage = float(input("What is the percentage of the tip? "))
+people = int(input("What is the number of people? "))
+
+# Calculate the tip
+totalBill = (bill + (bill * (percentage/100)))
+individualpay = totalBill/people
+
+print(f"The total bill is ${round(totalBill, 2)} 
+      and the amount to be paid by each individual is ${round(individualpay, 2)}")
