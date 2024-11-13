159041025bb4163753b296f58815fb1bb7f29182
EdwinTarus <85441814+EdwinTarus@users.noreply.github.com>
2024-11-13 03:29:18 +0300

    Create description.py

---
+++ Day 14 /description.py
@@ -0,0 +1,5 @@
+"""
+The "Higher or Lower" game is a guessing game where players are presented with two options and must guess which one has a higher number of a particular metric—in this case, Instagram followers. 
+For each round, a new account is shown alongside the previous one, and players guess whether it has more or fewer followers. Correct guesses keep the game going, while a wrong guess ends it, 
+challenging players to reach the highest streak possible.
+"""
